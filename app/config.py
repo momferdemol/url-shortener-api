3e08f3375671dc5791e03da8dbdd24064cc23846
Momfer de Mol <93478412+momferdemol@users.noreply.github.com>
2023-11-19 13:38:23 +0100

    fixes

--- app/config.py
+++ app/config.py
@@ -7,14 +7,14 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     env_name: str = "local"
-    base_url: str = "http://localhost:80"
+    base_url: str = "http://localhost:8000"
     db_url: str = "sqlite:///./app/shortener.db"
 
     class Config:
         env_file = ".env"
 
 
-@lru_cache
+# @lru_cache
 def get_settings() -> Settings:
     settings = Settings()
     print(f"Loading settings for: {settings.env_name}")
